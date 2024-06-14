c677ef3836800018459cf31d732331e0c100e068
gooooog <2412248477@qq.com>
2024-06-14 21:39:27 +0800

    更改

--- database.h
+++ database.h
@@ -2,7 +2,48 @@
 // Created by Defender on 2024/6/14.
 //
 
-#ifndef SDK_PROJECT__DATABASE_H_
-#define SDK_PROJECT__DATABASE_H_
+#ifndef CodeCraftSDK_DATABASE_H_
+#define CodeCraftSDK_DATABASE_H_
 
-#endif // SDK_PROJECT__DATABASE_H_
+#include<iostream>
+#include<vector>
+
+class job{
+public:
+  int id;
+  int task_size;
+  int output_data_size;
+
+  std::vector<int> data_dependence;
+  std::vector<int> affinity_machine;
+
+};
+
+class machine{
+public:
+  int id;
+  int power;
+
+};
+
+class disk{
+public:
+  int id;
+  int speed;
+  int quota;
+
+  int current_usage;
+
+};
+
+class task{
+public:
+  std::vector<job*> job;
+  std::vector<machine*> machine;
+  std::vector<disk*> disk;
+
+  std::vector<int> environment_dependence;
+
+
+};
+#endif // CodeCraftSDK_DATABASE_H_
--- parser.h
+++ parser.h
@@ -2,7 +2,7 @@
 // Created by Defender on 2024/6/14.
 //
 
-#ifndef SDK_PROJECT__PARSER_H_
-#define SDK_PROJECT__PARSER_H_
+#ifndef CodeCraftSDK_PARSER_H_
+#define CodeCraftSDK_PARSER_H_
 
-#endif // SDK_PROJECT__PARSER_H_
+#endif // CodeCraftSDK_PARSER_H_
